0fa3585acd7e141ac627930d303a7991b5f87812
PatrickVongsa <patrick.vongsa@gmail.com>
2022-09-27 10:02:28 +0200

    layout steup

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StyleSheet, SafeAreaView, StatusBar, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.safeView}> {/* SafeAreaView avoid the notch and status bar and navigation bar on Iphone*/}
+        <View style={styles.searchContainer}>
+          <Text>our react native blank canvas</Text>
+        </View>
+        <View style={styles.listContainer}>
+          <Text>our react native blank canvas</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeView: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight, /* allow to avoid the android status */
+  },
+  searchContainer: {
+    padding: 16,
+    backgroundColor: "green",
+  },
+  listContainer: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "green",
   },
 });
