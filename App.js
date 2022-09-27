d9703c3c1805c676ef775b241a47f56a49f7442a
PatrickVongsa <patrick.vongsa@gmail.com>
2022-09-27 14:41:18 +0200

    styled component implemented

--- App.js
+++ App.js
@@ -1,41 +1,12 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { StyleSheet, SafeAreaView, StatusBar, Text, View } from "react-native";
-
-import { Searchbar } from "react-native-paper";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.safeView}>
-        {/* SafeAreaView avoid the notch and status bar and navigation bar on Iphone*/}
-        <View style={styles.searchContainer}>
-          <Searchbar
-            placeholder="Search"
-            // onChangeText={onChangeSearch}
-            // value={searchQuery}
-          />
-        </View>
-        <View style={styles.listContainer}>
-          <Text>our react native blank canvas</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  safeView: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight /* allow to avoid the android status */,
-  },
-  searchContainer: {
-    padding: 16,
-  },
-  listContainer: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});
--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Master project from my udemy course
 
 ## Branches
 
-1-app-layout : setup the app layout
-2-eslint-config : setup eslint and prettier
-3-paper-searchbar : add searchbar from react-native-paper
+- 1-app-layout : setup the app layout
+- 2-eslint-config : setup eslint and prettier
+- 3-paper-searchbar : add searchbar from react-native-paper
+- 4-list-restaurant : add a list of restaurants
