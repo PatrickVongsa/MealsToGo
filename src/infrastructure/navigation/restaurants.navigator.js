a625fdb4eaa68988c4d168a03f407e09ab67fd10
PatrickVongsa <patrick.vongsa@gmail.com>
2022-09-28 16:07:31 +0200

    navigation optimized done

---
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Text } from "react-native-paper";
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from "@react-navigation/stack";
+
+import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
+
+const RestaurantStack = createStackNavigator();
+
+export const RestaurantsNavigator = () => {
+  return (
+    <RestaurantStack.Navigator
+      screenOptions={{
+        headerShown: false,
+        ...TransitionPresets.ModalPresentationIOS,
+        ...TransitionPresets.ModalTransition,
+      }}
+    >
+      <RestaurantStack.Screen
+        name="Restaurants"
+        component={RestaurantsScreen}
+      />
+      <RestaurantStack.Screen
+        name="RestaurantDetail"
+        component={() => <Text>restaurant detail</Text>}
+      />
+    </RestaurantStack.Navigator>
+  );
+};
